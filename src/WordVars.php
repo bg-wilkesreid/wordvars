3507c16c1747ba21e8c3f7a1132368cc27ead623
Samuel Reid <Sam@Sams-Mac.local>
2017-08-10 10:42:51 -0500

    Adding all initial features

--- src/WordVars.php
+++ src/WordVars.php
@@ -4,7 +4,43 @@ namespace WordVars;
 
 class WordVars
 {
-  public function hello() {
-    return "hello, world";
+  private $templatepath;
+  private $outputpath;
+  private $codes;
+
+  public function __construct($templatepath = "template.docx", $outputpath = "output.docx") {
+    $this->templatepath = $templatepath;
+    $thix->outputpath = $outputpath;
+  }
+
+  public function setCodes($codes) {
+    $this->codes = $codes;
+  }
+
+  public function setCalculatedCodes($calculated_codes) {
+    foreach ($calculated_codes as $calculated_code=>$calcvalue) {
+      foreach ($this->codes as $code=>$codevalue) {
+        $calcvalue = preg_replace('/#\['.$code.'\]/', $codevalue, $calcvalue);
+      }
+      $this->codes[$calculated_code] = $calcvalue;
+    }
+  }
+
+  public function go() {
+    copy($this->templatepath, $this->outputpath);
+    $zip = new ZipArchive;
+    $zip->open($this->outputpath);
+
+    $content = $zip->getFromName('word/document.xml');
+
+    // Do replacement
+    foreach ($this->codes as $code=>$value) {
+      $content = preg_replace('%#\[(?:</w:t></w:r>(?:<w:proofErr[^/]*/>)?<w:r[^>]*><w:t[^>]*>)?'.$code.'(?:</w:t></w:r><w:proofErr[^>]*/><w:r[^>]*><w:t[^>]*>)?\]%', $value, $str);
+    }
+
+    $zip->deleteName('word/document.xml');
+    $zip->addFromString('word/document.xml', $str);
+
+    $zip->close();
   }
 }
